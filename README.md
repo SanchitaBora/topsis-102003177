11a80a08a39484829c333227ac119c59c578b23f
Sanchita Bora <74770301+SanchitaBora@users.noreply.github.com>
2023-01-23 07:38:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Topsis-Sanchita-102003177
+
+## Installation
+```pip install saral-square```
+
+## How to use it?
+Open terminal and type topsis-sanchita-102003177 and then input 
+```topsis <script_name> <input_data_file_name> <wt> <imp> <result_file_name>```
+
+## License
+
+© 2020 Udit Vashisht
+
+This repository is licensed under the MIT license. See LICENSE for details.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="topsis-sanchita-102003177",
+    version="1.0.0",
+    description="It squares the number",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uditvashisht/saral-square",
+    author="Sanchita Bora",
+    author_email="sbora_be20@thapar.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis-Sanchita-102003177.102003177:main",
+        ]
+    },
+)
